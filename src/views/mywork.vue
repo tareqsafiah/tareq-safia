95d2d8f7069a9334d8cdda3b685b0bac6487b2d3
tareqsafiah <93489383+tareqsafiah@users.noreply.github.com>
2021-12-27 11:24:14 +0200

    Update mywork.vue

--- src/views/mywork.vue
+++ src/views/mywork.vue
@@ -1,7 +1,7 @@
 <template>
 <v-app id="mywork">
           <navbar id="nav"/>
-<div class="continer ml-3 mb-2 " data-v-1975891f="">
+<div class="container-fluid">
  <v-card>
     <v-tabs
       color="deep-purple accent-4"
@@ -12,277 +12,121 @@
         v-for="n in 3"
         :key="n"
       >
-      <div id="gallery" data-v-63bb9663="" data-v-1975891f="">
-      <div class="continer" data-v-63bb9663="">
-        <div class="row"  data-v-63bb9663="" warp="">
-<div class="up-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-            <v-img
-              src="../assets/01_preview_image.__large_preview.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-  <!-- <v-img
-        src="../assets/01_preview_image.__large_preview.jpg"
-        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-      ></v-img> -->
-  </div>
-<div class="down-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img src="../assets/preview.jpg">
-        <div class="fill-height bottom-gradient"></div>
-      </v-img> -->
-      <v-img
-              src="../assets/preview.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-</div>
-<div class="up-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img src="../assets/25-best-website-builder-for-online-clothing-stores-2020-1.jpg">
-        <div class="fill-height repeating-gradient"></div>
-      </v-img> -->
-       <v-img
-              src="../assets/25-best-website-builder-for-online-clothing-stores-2020-1.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-</div>
+      <div id="gallery">
+      <div class="container">
+        <div class="row" warp="">
+<div class="up-2-anim col-sm-6 col-md-4 col-12">
+  <div class="v-card v-sheet theme--light" style="max-width: 400px;">
+    <div class="v-image v-responsive white--text align-end theme--light" style="height: 200px;">
+      <div class="v-responsive__sizer" style="padding-bottom: 56.23%;">
         </div>
-        <div class="row"  data-v-63bb9663="" warp="">
-    <div class="up-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img
-        src="../assets/divi-cleaning-company-layout-pack-featured-image.jpg"
-        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-      ></v-img> -->
-       <v-img
-              src="../assets/divi-cleaning-company-layout-pack-featured-image.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
+         <div class="v-image__image v-image__image--cover">
+        <v-img
+      src="../assets/mazadak.jpg"/></div>
+        <div class="v-responsive__content" style="width: 626px;">
+          </div></div><div class="v-card__subtitle pb-0">Mazadak Website Multi Page</div>
+          <div class="v-card__text text--primary"><div>To meet the market's need for a virtual auction</div>
+          </div><div class="v-card__actions">
+            <div class="row justify-center">
+              <div class="col col-4">
+                <button type="button" class="blak--text v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default red lighten-5" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="v-btn__content"><a href=" " style="text-decoration: none; color: black;">Visit</a><i aria-hidden="true" class="v-icon notranslate v-icon--right mdi mdi-arrow-right-bold-circle theme--light"></i></span></button>
+<div role="dialog" class="v-dialog__container"></div></div></div></div></div></div>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
+<div class="down-2-anim col-sm-6 col-md-4 col-12">
+  <div class="v-card v-sheet theme--light" style="max-width: 400px;">
+    <div class="v-image v-responsive white--text align-end theme--light" style="height: 200px;">
+      <div class="v-responsive__sizer" style="padding-bottom: 56.23%;">
+        </div>
+        <div class="v-image__image v-image__image--cover" style="background-image: url('file:///D:/vue/design_ui/src/assets/mercedes.jpg;'); background-position: center center;">
+        <v-img
+              src="../assets/mercedes.jpg"/>
+        </div>
+        <div class="v-responsive__content" style="width: 626px;">
+          </div></div><div class="v-card__subtitle pb-0">Mercedes website One Page</div>
+          <div class="v-card__text text--primary"><div>Just For Training</div>
+          </div><div class="v-card__actions">
+            <div class="row justify-center">
+              <div class="col col-4">
+                <button type="button" class="blak--text v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default red lighten-5" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="v-btn__content" ><a href=" " style="text-decoration: none; color: black;">Visit</a><i aria-hidden="true" class="v-icon notranslate v-icon--right mdi mdi-arrow-right-bold-circle theme--light"></i></span></button>
+<div role="dialog" class="v-dialog__container"></div></div></div></div></div></div>
 
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
+<div class="up-2-anim col-sm-6 col-md-4 col-12">
+  <div class="v-card v-sheet theme--light" style="max-width: 400px;">
+    <div class="v-image v-responsive white--text align-end theme--light" style="height: 200px;">
+      <div class="v-responsive__sizer" style="padding-bottom: 56.23%;">
+        </div>
+        <div class="v-image__image v-image__image--cover">
+        <v-img
+              src="../assets/cvgenrator.jpg"/>
+        </div>
+        <div class="v-responsive__content" style="width: 626px;">
+          </div></div><div class="v-card__subtitle pb-0">Cv Genrator website </div>
+          <div class="v-card__text text--primary"><div>Free CV Genrator to apply for job</div>
+          </div><div class="v-card__actions">
+            <div class="row justify-center">
+              <div class="col col-4">
+                <button type="button" class="blak--text v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default red lighten-5" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="v-btn__content" ><a href=" " style="text-decoration: none; color: black;">Visit</a><i aria-hidden="true" class="v-icon notranslate v-icon--right mdi mdi-arrow-right-bold-circle theme--light"></i></span></button>
+<div role="dialog" class="v-dialog__container"></div></div></div></div></div></div>
 
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
+<div class="up-2-anim col-sm-6 col-md-4 col-12">
+  <div class="v-card v-sheet theme--light" style="max-width: 400px;">
+    <div class="v-image v-responsive white--text align-end theme--light" style="height: 200px;">
+      <div class="v-responsive__sizer" style="padding-bottom: 56.23%;">
+        </div>
+        <div class="v-image__image v-image__image--cover">
+        <v-img
+              src="../assets/notes-6399119_1920.jpg"/>
+        </div>
+        <div class="v-responsive__content" style="width: 626px;">
+          </div></div><div class="v-card__subtitle pb-0"> To Do List </div>
+          <div class="v-card__text text--primary"><div>To Sort Your Mission</div>
+          </div><div class="v-card__actions">
+            <div class="row justify-center">
+              <div class="col col-4">
+                <button type="button" class="blak--text v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default red lighten-5" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="v-btn__content" ><a href=" " style="text-decoration: none; color: black;">Visit</a><i aria-hidden="true" class="v-icon notranslate v-icon--right mdi mdi-arrow-right-bold-circle theme--light"></i></span></button>
+<div role="dialog" class="v-dialog__container"></div></div></div></div></div></div>
 
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-    </div>
-    <div class="down-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img src="../assets/divi-laundry-service-layout-pack-featured-image.jpg">
-        <div class="fill-height bottom-gradient"></div>
-      </v-img> -->
-       <v-img
-              src="../assets/divi-laundry-service-layout-pack-featured-image.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
+<div class="down-2-anim col-sm-6 col-md-4 col-12">
+  <div class="v-card v-sheet theme--light" style="max-width: 400px;">
+    <div class="v-image v-responsive white--text align-end theme--light" style="height: 200px;">
+      <div class="v-responsive__sizer" style="padding-bottom: 56.23%;">
+        </div>
+        <div class="v-image__image v-image__image--cover">
+        <v-img
+              src="../assets/final.jpg"/>
+        </div>
+        <div class="v-responsive__content" style="width: 626px;">
+          </div></div><div class="v-card__subtitle pb-0">Million's Way </div>
+          <div class="v-card__text text--primary"><div>My First Blog</div>
+          </div><div class="v-card__actions">
+            <div class="row justify-center">
+              <div class="col col-4">
+                <button type="button" class="blak--text v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default red lighten-5" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="v-btn__content" ><a href=" " style="text-decoration: none; color: black;">Visit</a><i aria-hidden="true" class="v-icon notranslate v-icon--right mdi mdi-arrow-right-bold-circle theme--light"></i></span></button>
+<div role="dialog" class="v-dialog__container"></div></div></div></div></div></div>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-    </div>
-   <div class="up-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img src="../assets/Free-Law-Firm-Website-Template-PSD.jpg">
-        <div class="fill-height repeating-gradient"></div>
-      </v-img> -->
-       <v-img
-              src="../assets/Free-Law-Firm-Website-Template-PSD.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-    </div>
-  </div>
-  <div class="row"  data-v-63bb9663="" warp="">
-    <div class="up-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img
-        src="../assets/restaurant-opening-web-template-design_23-2148410411.jpg"
-        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-      ></v-img> -->
-       <v-img
-              src="../assets/restaurant-opening-web-template-design_23-2148410411.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-    </div>
-   <div class="down-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img src="../assets/seo-responsive.png">
-        <div class="fill-height bottom-gradient"></div>
-      </v-img> -->
-       <v-img
-              src="../assets/seo-responsive.png"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-    </div>
-    <div class="up-2-anim col-sm-6 col-md-4 col-12" data-v-63bb9663="">
-  <div class="v-responsive__sizer"></div>
-      <!-- <v-img src="../assets/template-landing-page-with-laptop_23-2148324571.jpg">
-        <div class="fill-height repeating-gradient"></div>
-      </v-img> -->
-       <v-img
-              src="@/assets/ccapture.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="450px"
-            >
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-    </div>
+<div class="up-2-anim col-sm-6 col-md-4 col-12">
+  <div class="v-card v-sheet theme--light" style="max-width: 400px;">
+    <div class="v-image v-responsive white--text align-end theme--light" style="height: 200px;">
+      <div class="v-responsive__sizer" style="padding-bottom: 56.23%;">
+        </div>
+        <div class="v-image__image v-image__image--cover">
+        <v-img
+              src="../assets/Capturen.jpg"/>
+        </div>
+        <div class="v-responsive__content" style="width: 626px;">
+          </div></div><div class="v-card__subtitle pb-0"> Comming Soon </div>
+          <div class="v-card__text text--primary"><div>Comming Soon</div>
+          </div><div class="v-card__actions">
+            <div class="row justify-center">
+              <div class="col col-4">
+                <button type="button" class="blak--text v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default red lighten-5" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="v-btn__content" ><a href=" " style="text-decoration: none; color: black;">Visit</a><i aria-hidden="true" class="v-icon notranslate v-icon--right mdi mdi-arrow-right-bold-circle theme--light"></i></span></button>
+<div role="dialog" class="v-dialog__container"></div></div></div></div></div></div>
   </div>
         </div>
       </div>
@@ -308,7 +152,7 @@ export default {
   top:0;
   z-index: 10;
 }
-.continer{
+body{
 background-color: #dfe6e9;
     opacity: 0.99;
 height: 100%;
@@ -342,5 +186,17 @@ height: 100%;
     opacity: 1;
     transform: translateY(0);
 }
+}
+.col-md-4 {
+    flex: 0 0 33.3333333333%;
+    width: 33.3333333333%;
+}
+.col-sm-6 {
+    flex: 0 0 50%;
+    width: 50%;
+}
+.col-12 {
+    flex: 0 0 100%;
+    width: 100%;
 }
 </style>
